a083fd564f91ab8643f5d93952f9f73f06e0a7ac
Benegripe <fabiolima39@gmail.com>
2016-03-23 23:17:33 -0300

    script to install pathogen

--- Vim/install.sh
+++ Vim/install.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#this file need stay in home directory
 
-cd ~/.vim/
-echo "execute pathogen#infect()
-syntax on
-filetype plugin indent on" >> .vimrc
+#install PATHOGEN
+folder=".vim/autoload .vim/bundle"
+mkdir -p $folder
 
-mkdir ~/.vim/bundle
-cd ~/.vim/bundle && \
-git clone git://github.com/tpope/vim-sensible.git
+pathogen="curl -LSso .vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim"
+$pathogen
+
+file=".vim/vimrc"
+string="execute pathogen#infect()"
+echo $string >> $file
+string="syntax on"
+echo $string >> $file
+string="filetype plugin indent on"
+echo $string >> $file
+
+path=".vim/bundle"
+cd $path
+
+git="git clone git://github.com/tpope/vim-sensible.git"
+$git
+
+#install PYTHON-MODE
