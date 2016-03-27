1ee9ff11b89e50846989825179dc1353db5281f5
Benegripe <fabiolima39@gmail.com>
2016-03-26 23:30:46 -0300

    more configuration to tmux

--- Tmux/install.sh
+++ Tmux/install.sh
@@ -2,5 +2,18 @@
 
 #this file must be in home directory
 file=".tmux.conf"
+#prefix to ctr + a
 echo "set -g prefix C-a" >> $file
+#star index window begin 1, ctr + a + number
 echo "set -g base-index 1" >> $file
+#index pane start with 1, ctr + a + t + number
+echo "setw -g pane-base index 1" >> $file
+#reload configuration file, ctrl + r
+echo "bind r source-file ~/.tmux.conf \; display \"Reloaded\"" >> $file
+#send command ctr + a to other application
+echo "bind C-a send-prefix" >> $file
+#split pane in vertical
+echo "bind | split-window -h" >> $file
+#split pane in horizontal
+echo "bind | split-window -v" >> $file
+
