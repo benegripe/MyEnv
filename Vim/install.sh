285ab5941a5dbfbfec610bd152fa3b7988714a73
LimaFabio <fabiolima39@gmail.com>
2016-04-01 22:01:39 -0300

    script install plugin nerdtree in vim

--- Vim/install.sh
+++ Vim/install.sh
@@ -2,30 +2,12 @@
 
 #this file need stay in home directory
 
-dir=".vim"
-if [ -d $dir ]; then
 	#install PATHOGEN
 	folder=".vim/autoload .vim/bundle"
 	mkdir -p $folder
 
 	pathogen="curl -LSso .vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim"
 	$pathogen
-
-	file=".vim/vimrc"
-	string="filetype off"
-	echo $string >> $file
-
-	string="call pathogen#infect()"
-	echo $string >> $file
-
-	string="call pathogen#helptags()"
-	echo $string >> $file
-
-	string="filetype plugin indent on"
-	echo $string >> $file
-
-	string="syntax on"
-	echo $string >> $file
 
 	path=".vim/bundle"
 	cd $path
@@ -36,7 +18,5 @@ if [ -d $dir ]; then
 	git="git clone https://github.com/klen/python-mode.git"
 	$git
 
-else
-	echo "Need install vim"
-fi
-
+	git="git clone https://github.com/scrooloose/nerdtree.git"
+	$git
