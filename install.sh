34287886300bc07555746348def13104c10aefce
fabio <fabiolima39@gmail.com>
2016-09-07 15:19:51 -0300

    script support to Debian

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/
 
-# Install package in ubuntu or Arch Linux
-dist_os=$(lsb_release -si)
+# Script to install package in Ubuntu and Debian vim,git,ctags,wget,tmux
+
 pac=''
+tools=('vim' 'git' 'ctags' 'tmux','wget')
+dist_os=$(lsb_release -si)
 if [ "$dist_os" = "Ubuntu" ];  then
-	tools=('vim' 'git' 'ctags' 'tmux','wget')
+	pac="sudo apt-get install"
+elif [ "$dist_os" = "Debian" ];  then
 	pac="apt-get install"
-	for i in "${tools[@]}"
-	do
-		sudo $pac $i
-	done
-else
-	echo "not support to this Operating System"
 fi
+for i in "${tools[@]}"
+do
+	sudo $pac $i
+done
