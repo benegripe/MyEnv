48b01800f515a1f9321ea92bb080c305722499fb
limafabio <fabiolima39@gmail.com>
2017-01-23 23:57:50 -0200

    refactor script install tools

--- install.sh
+++ install.sh
@@ -1,17 +1,62 @@
-#!/usr/bin/
+#!/usr/bin
+#solved by FabioLima
+#
+#NameScript:	install2.sh
+#
+#Author and Maintaining: Fabio Lima
+#
+#-----------------------------------
+#Description:
+# Script to install package in Debian vim,git,ctags,wget,tmux
+#
+#-----------------------------------
+#
+#Example:
+# ./install.sh
+#
+#-----------------------------------
+#
+#History
+#
+#v1.0 2017/01/22, FabioLima
+#
+#-----------------------------------
+#
+#License: GPL
+#
+	
+#FlagDebug
+set -u
+set -e
+set -o pipefail
+	
+Debug(){
+	[ $1 -le $DEBUG ] && echo "--- DEBUG $*" 
+}
 
-# Script to install package in Ubuntu and Debian vim,git,ctags,wget,tmux
+function installTools(){
+	declare -a tools=('vim' 'git' 'ctags' 'tmux' 'wget')
+	lenght=`expr ${#tools[@]} - 1`
+	for i in `seq 0 $lenght`;
+	do
+		echo "install ${tools[${i}]} Y/n ?"
+		read answer
+		if [ "$answer" = "Y" ] || [ "$answer" = "y" ];
+		then
+			install=$pac${tools[${i}]}
+			$install
+		fi
+	done
+}
+
+function getDistro(){
+	dist_os=$(lsb_release -si)
+	if [ "$dist_os" = "Debian" ];  then
+		pac="sudo apt-get install -y "
+	fi
+}
 
 pac=''
-tools=('vim' 'git' 'ctags' 'tmux','wget')
-dist_os=$(lsb_release -si)
-if [ "$dist_os" = "Ubuntu" ];  then
-	pac="sudo apt-get install"
-	path_home="/home/$(whoami)"
-elif [ "$dist_os" = "Debian" ];  then
-	pac="apt-get install"
-fi
-for i in "${tools[@]}"
-do
-	$pac $i
-done
+tools=''
+getDistro
+installTools
